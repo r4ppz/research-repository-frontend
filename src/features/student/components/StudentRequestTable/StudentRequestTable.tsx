68429a101840656fdcd8592cc0264f0b29342d49
r4ppz <johnreyrabosa.f@gmail.com>
2026-01-18 08:51:40 +0800

    refractor(StudentRequestTable): improve structure

--- src/features/student/components/StudentRequestTable/StudentRequestTable.tsx
+++ src/features/student/components/StudentRequestTable/StudentRequestTable.tsx
@@ -1,3 +1,4 @@
+import type { PaginationState } from "@tanstack/react-table";
 import { DataTable } from "@/components/common/DataTable/DataTable";
 import { LoadingSpinner } from "@/components/common/LoadingSpinner/LoadingSpinner";
 import { useUserRequests } from "../../hooks/useUserRequests";
@@ -28,8 +29,14 @@ export function StudentRequestTable() {
       pageCount={pageCount}
       pagination={{ pageIndex, pageSize }}
       onPaginationChange={(updater) => {
-        const nextState =
-          typeof updater === "function" ? updater({ pageIndex, pageSize }) : updater;
+        let nextState: PaginationState;
+
+        if (typeof updater === "function") {
+          nextState = updater({ pageIndex, pageSize });
+        } else {
+          nextState = updater;
+        }
+
         setPageIndex(nextState.pageIndex);
       }}
       meta={tableMeta}
