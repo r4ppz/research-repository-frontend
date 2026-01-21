243705fe70099e14c3d7001b82e1872b1bf99025
r4ppz <johnreyrabosa.f@gmail.com>
2026-01-21 11:06:46 +0800

    feat: migrate from Biome to ESLint + Prettier + Stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,11 @@ import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// biome-ignore lint/style/noDefaultExport: < Vite needs default here ;P >
 export default defineConfig(({ mode }) => {
   const isDevelopment = mode === "development";
 
   return {
-    plugins: [
-      react({
-        babel: {
-          plugins: [["babel-plugin-react-compiler", { target: "19" }]],
-        },
-      }),
-    ],
+    plugins: [react()],
     preview: {
       allowedHosts: true, // for testing
     },
