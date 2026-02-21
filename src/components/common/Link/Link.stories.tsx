ed2a3031dad37a8738dfcb76ce671170a6e8fb92
r4ppz <johnreyrabosa.f@gmail.com>
2026-02-21 10:17:33 +0800

    style(Link component): stories n fix css

--- src/components/common/Link/Link.stories.tsx
+++ src/components/common/Link/Link.stories.tsx
@@ -4,10 +4,26 @@ import { Link } from "./Link";
 
 const meta = {
   component: Link,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    href: {
+      control: "text",
+    },
+    children: {
+      control: "text",
+    },
+  },
 } satisfies Meta<typeof Link>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    href: "https://r4ppz.github.io/research-repo-docs/",
+    children: "This is a link",
+  },
+};
