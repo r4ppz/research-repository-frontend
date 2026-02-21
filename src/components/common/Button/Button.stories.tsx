833290d85006d9f5c838da60cf1a9dae47149680
r4ppz <johnreyrabosa.f@gmail.com>
2026-02-21 10:21:59 +0800

    style(Button component): use css variable for diable state and icon

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -31,13 +31,13 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     variant: "primary",
-    children: "Primary",
+    children: "Button",
   },
 };
 
 export const Secondary: Story = {
   args: {
     variant: "secondary",
-    children: "Secondary",
+    children: "Button",
   },
 };
